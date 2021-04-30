4423196e10a782d5b3da1919af654bc5f8e0977e
Gordon Watts <gwatts@uw.edu>
2021-04-30 08:06:35 -0700

    First draft of dummy open data finder

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # servicex_did_demo
  Demo the ServiceX DID Library
+
+## Usage
+
+This is a demo DID finder for the ServiceX project. It can be run in ServiceX and if given an appropriate dataset name, it will return a fixed set of data files to the system.
+
+It is meant to demo usage of the `ServiceX_DID` library, and be used as a template to build new ServiceX DID finders.
+
+## Testing Demo in ServiceX
+
+To test this in ServiceX perform the following steps:
+
+## Creating a new ServiceX DID Finder
+
+To create a new DID finder from this template, follow these steps:
+
---
+++ src/demo_did.py
@@ -0,0 +1,35 @@
+from typing import Any, AsyncGenerator, Dict
+from servicex_did import start_did_finder
+import logging
+
+async def find_files(did_name: str) -> AsyncGenerator[Dict[str, Any], None]:
+    '''For each incoming did name, generate a list of files that ServiceX can process
+
+    Args:
+        did_name (str): Dataset name
+
+    Returns:
+        AsyncGenerator[Dict[str, any], None]: yield each file
+    '''
+    if did_name != 'dataset1':
+        raise Exception(f'Dataset "{did_name}" is not known!')
+
+    # TODO: convert http to root from cern open data for testing
+    yield {
+        'file_path': "http://opendata.cern.ch/record/3827/files/mc_167740.WenuWithB.root",  # Path accessible via transformers (root, http)
+        'adler32': 0,  # No clue
+        'file_size': 0,  # Size in bytes if known
+        'file_events': 0,  # Number of events if known
+    }
+
+def run_demo():
+    log = logging.getLogger(__name__)
+
+    try:
+        log.info('Starting demo DID finder')
+        start_did_finder('demo', find_files)
+    finally:
+        log.info('Done running demo DID finder')
+
+if __name__ == "__main__":
+    run_demo()
